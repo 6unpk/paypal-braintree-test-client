8b27e8b5db9c15bacf03084363ab498a355fac5a
Junu Park <pkjoho95@gmail.com>
2023-07-31 14:44:29 +0900

    id select

--- src/PaypleClient.js
+++ src/PaypleClient.js
@@ -2,6 +2,7 @@ import { useEffect, useState } from "react";
 
 export default function PaypleClient() {
     const [token, setToken] = useState('');
+    const [myWorkIdText, setMyWorkIdTedxt] = useState('');
 
     const fetchPaymentToken = async () => {
         const token = await fetch('https://api.dev.pixohq.com/LOGO_SHOP/payments/token', {
@@ -20,6 +21,9 @@ export default function PaypleClient() {
     }, []);
 
     const onOrderSubmit = () => {
+        if (myWorkIdText === '') {
+            return;
+        }
         let obj = new Object();
 
         // from server generated
@@ -37,7 +41,12 @@ export default function PaypleClient() {
 		obj.resultUrl = "https://api.dev.pixohq.com/LOGO_SHOP/payments/orders";
 		obj.isDirect = "Y";
         // obj.serviceDefine = '12341';
-        obj.serviceDefine = btoa(JSON.stringify({myWorkId: "f4f423ee-c540-453f-b67b-e5fb3d8be9fc", userId: "7d81c708-7011-4ac8-8489-e378e64f9a5e"}));
+        obj.serviceDefine = btoa(JSON.stringify(
+        {
+            myWorkId: myWorkIdText,
+            userId: "7d81c708-7011-4ac8-8489-e378e64f9a5e",
+            failedRedirectUrl: 'http://localhost:5174/payple'
+        }));
         window.paypleGpayPaymentRequest(obj);
     }
 
@@ -59,13 +68,20 @@ export default function PaypleClient() {
 		obj.resultUrl = "https://api.dev.pixohq.com/LOGO_SHOP/payments/subscriptions";
 		obj.isDirect = "Y";
         // obj.serviceDefine = '12341';
-        obj.serviceDefine = btoa(JSON.stringify({myWorkId: "f4f423ee-c540-453f-b67b-e5fb3d8be9fc", userId: "7d81c708-7011-4ac8-8489-e378e64f9a5e"}));
+        obj.serviceDefine = btoa(JSON.stringify({
+            planId: "b7xm", 
+            userId: "7d81c708-7011-4ac8-8489-e378e64f9a5e",
+            failedRedirectUrl: 'http://localhost:5174/payple'
+        }));
         window.paypleGpayPaymentRequest(obj);
     }
 
     return (
-        <div>
-            <div style={{ display: 'flex', justifyContent: 'center', alignItems: 'center', height: '100vh' }}>
+        <div style={{ display: 'flex', flexDirection: 'column', justifyContent: 'center', alignItems: 'center', height: '100vh' }}>
+            <div style={{ display: 'flex', margin: '10px' }}>
+                <input type="text"placeholder="MyWorkId" style={{ width: '300px', height: '44px', fontSize: '22px' }} value={myWorkIdText} onChange={(e) => setMyWorkIdTedxt(e.target.value)} />
+            </div>
+            <div style={{ display: 'flex', flexDirection: 'row' }}>
                 <button onClick={onOrderSubmit} style={{ width: '100px', height: '100px', border: 'none' }}>단건 결제 진행</button>
                 <button onClick={onSubscriptionSubmit} style={{ width: '100px', height: '100px', border: 'none' }}>구독 결제 진행</button>
             </div>
